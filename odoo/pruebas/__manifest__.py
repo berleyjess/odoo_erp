4f78f99296874fb5ddb815cbc12e4f8e683ae146
davidsotelo229384 <david.sotelo229384@potros.itson.edu.mx>
2025-08-06 12:47:56 -0700

    vista solo lector y edicion, problema con generacion de pdf

--- odoo/pruebas/__manifest__.py
+++ odoo/pruebas/__manifest__.py
@@ -6,7 +6,7 @@
     'version': '1.0',
     'depends': ['base'],
     'data': [
-        'views/prueba.xml',
+        'views/pruebapersona.xml',
     ],
     'installable': True,
     'application': True,
